f58c42c6049adb2725de43bd32ea9ce0c6c007ca
javiprada1 <javiprada1@gmail.com>
2017-04-19 17:51:40 +0200

    Últimos ejercicios de Herencia

--- Ejercios Python - Curso Odoo/herencia.py
+++ Ejercios Python - Curso Odoo/herencia.py
@@ -7,8 +7,23 @@ class Instrumento:
 	def romper(self):
 		print "Eso lo pagas tu"
 		print "Son "+str(self.precio)+" euros"
-class Trompeta(Instrumento): #Trompeta tiene el método tocar y romper
-	pass
-class Guitarra(Instrumento): #Trompeta tiene el método tocar y romper
+class Trompeta(Instrumento): #Trompeta tiene el metodo tocar y romper
+	def __init__(self,precio):
+		self.precio = precio
+		print "La trompeta cuesta"+str(precio)
+
+	def tocar_trompeta(self):
+		print "Estamos tocando la trompeta"
+
+class Guitarra(Instrumento): #Trompeta tiene el metodo tocar y romper
 	pass
 
+b=Trompeta(50) #Inicializa Trompeta
+b.tocar_trompeta() #Imprime estamos tocando la trompeta
+b.romper() #Imprime eso lo pagas tu. Son 4 euros. Metodo Heredado
+
+#Python permite la herencia multiple
+# Class D(C,B)
+# print D.__mro__
+#
+#
