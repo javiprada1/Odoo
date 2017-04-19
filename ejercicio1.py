cf0d37e75bbc6047a92552fa861af8c8f575ab8c
javiprada1 <javiprada1@gmail.com>
2017-04-19 15:24:31 +0100

    Listado de ejercicios del primer día

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+print "Ejercicio 1"
+nombres = ["Maria","Alberto","Ana","Pedro","Rosa","Juan"]
+print "Los nombres de chicos son: "+nombres[1]+", "+nombres[3]+", "+nombres[5]
+print "Los nombres de chicas son: "+nombres[0]+", "+nombres[2]+", "+nombres[4]
+
+print "Ejercicio 2"
+INVENTORY = {'gold':500,'pouch':['flint', 'twine','gemstone'],'backpack':['xylophone','dagger','bedroll','bread loaf']}
+INVENTORY['backpack'].sort()
+INVENTORY['backpack'].remove('dagger')
+print INVENTORY
